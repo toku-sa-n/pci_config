6cf6dba504e263368683bf6bee60d75a7694fe42
toku-sa-n <tokusan441@gmail.com>
2020-09-25 22:25:40 +0900

    Define `Register::fetch`

--- src/space/raw.rs
+++ src/space/raw.rs
@@ -4,10 +4,24 @@
 
 use super::accessor::{Accessor, Bus, Device, Function, RegisterIndex};
 
-pub(crate) const NUM_REGISTERS: usize = 64;
-
-struct Registers([u32; NUM_REGISTERS]);
+struct Registers([u32; RegisterIndex::MAX]);
 impl Registers {
+    fn fetch(bus: Bus, device: Device) -> Option<Self> {
+        if !Self::exist(bus, device) {
+            None
+        } else {
+            let mut registers = [0u32; RegisterIndex::MAX];
+
+            for i in 0..RegisterIndex::MAX {
+                let accessor =
+                    Accessor::new(bus, device, Function::zero(), RegisterIndex::new(i as u8));
+                registers[i] = accessor.read();
+            }
+
+            Some(Self(registers))
+        }
+    }
+
     fn exist(bus: Bus, device: Device) -> bool {
         let accessor = Accessor::new(bus, device, Function::zero(), RegisterIndex::new(0));
         let ids = accessor.read();
