2d70629a10edfb8c58f9868c264c360498a30460
toku-sa-n <tokusan441@gmail.com>
2020-09-25 23:13:51 +0900

    Define `Id`

--- src/space/common/id.rs
+++ src/space/common/id.rs
@@ -2,6 +2,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+struct Id {
+    device: Device,
+    vendor: Vendor,
+}
+
 #[derive(Copy, Clone)]
 struct Device(u16);
 impl Device {
