e5a2f541522808e5933b01485c6f90bdda530fce
toku-sa-n <tokusan441@gmail.com>
2020-09-25 22:18:36 +0900

    Define `registers::exist`

--- src/space/raw.rs
+++ src/space/raw.rs
@@ -2,8 +2,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use super::accessor::{Bus, Device, RegisterIndex};
+use super::accessor::{Accessor, Bus, Device, Function, RegisterIndex};
 
 pub(crate) const NUM_REGISTERS: usize = 64;
 
 struct Registers([u32; NUM_REGISTERS]);
+impl Registers {
+    fn exist(bus: Bus, device: Device) -> bool {
+        let accessor = Accessor::new(bus, device, Function::zero(), RegisterIndex::new(0));
+        let ids = accessor.read();
+
+        ids != !0
+    }
+}
