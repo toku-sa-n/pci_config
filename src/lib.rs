d4409e1ac6311c045266bf2431bd3ea7172fbfc4
toku-sa-n <tokusan441@gmail.com>
2020-09-25 20:57:06 +0900

    Mark as `no_std`

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#![no_std]
+
 struct Bus(u8);
 impl Bus {
     fn new(bus: u8) -> Self {
