c6a2217d8006b7be472c491b9205f56eb5b03ad7
toku-sa-n <tokusan441@gmail.com>
2020-09-26 17:02:21 +0900

    Define `msi_x` module

--- src/space/header_spec/capability/register/spec/msi_x.rs
+++ src/space/header_spec/capability/register/spec/msi_x.rs
@@ -1,14 +1,3 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-mod msi;
-mod msi_x;
-
-#[derive(Copy, Clone)]
-struct MessageControl(u16);
-impl MessageControl {
-    fn new(control: u16) -> Self {
-        Self(control)
-    }
-}
