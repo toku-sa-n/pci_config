f3cda1024b8d57191982217f9329eeedf2c099d4
toku-sa-n <tokusan441@gmail.com>
2020-09-26 16:09:05 +0900

    Define `Id::parse_registers`

--- src/space/header_spec/capability.rs
+++ src/space/header_spec/capability.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use crate::space::accessor::RegisterIndex;
+use crate::space::{accessor::RegisterIndex, registers::Registers};
 
 define_field!(Pointer, u8, 0x0d, 0, 0xff);
 
@@ -10,9 +10,19 @@ struct Common {
     id: Id,
     next_ptr: NextPointer,
 }
+impl Common {
+    fn parse_registers(registers: &Registers, base: RegisterIndex) -> Self {
+        unimplemented!()
+    }
+}
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Default)]
 struct Id(u8);
+impl Id {
+    fn parse_registers(registers: &Registers, base: RegisterIndex) -> Self {
+        Self((registers[base] & 0xff) as u8)
+    }
+}
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Default)]
 struct NextPointer(RegisterIndex);
