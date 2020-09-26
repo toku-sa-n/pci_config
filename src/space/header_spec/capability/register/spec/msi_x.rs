99af6641d3059dad901d1171e55687e91656faf9
toku-sa-n <tokusan441@gmail.com>
2020-09-26 17:35:02 +0900

    Define `TypeSpecMsiX`

--- src/space/header_spec/capability/register/spec/msi_x.rs
+++ src/space/header_spec/capability/register/spec/msi_x.rs
@@ -3,9 +3,17 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use {
+    super::MessageControl,
     crate::space::header_spec::bar,
     os_units::{Bytes, Size},
 };
+
+struct TypeSpecMsiX {
+    bir: Bir,
+    table_offset: TableOffset,
+    pending_bit_bir: PendingBitBir,
+    pending_bit_offset: PendingBitOffset,
+}
 
 struct Bir(bar::Index);
 impl Bir {
