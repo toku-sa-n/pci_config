6b97ff384c9b56697077696461f83e508c6b7ba8
toku-sa-n <tokusan441@gmail.com>
2020-09-26 10:42:02 +0900

    Define `Device::parse_registers`

--- src/space/common/id.rs
+++ src/space/common/id.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use super::super::{accessor::RegisterIndex, registers::Registers};
+
 struct Id {
     device: Device,
     vendor: Vendor,
@@ -19,5 +21,10 @@ impl Id {
 }
 
 define_field!(Device, u16);
+impl Device {
+    fn parse_registers(registers: &Registers) -> Self {
+        Self::new(((registers[RegisterIndex::new(0)] >> 16) & 0xffff) as _)
+    }
+}
 define_field!(Vendor, u16);
 define_field!(Revision, u8);
