39ea4f5c75cc194d66aa988e37597acfd6cf300a
toku-sa-n <tokusan441@gmail.com>
2020-09-26 14:24:37 +0900

    Mark as `pub(crate)`

--- src/space/common/header.rs
+++ src/space/common/header.rs
@@ -4,7 +4,7 @@
 
 define_field!(HeaderType, u8, 3, 16, 0xff);
 impl HeaderType {
-    fn ty(self) -> Header {
+    pub(crate) fn ty(self) -> Header {
         match self.0 & 0x7f {
             0 => Header::Standard,
             1 => Header::PciToPciBridge,
@@ -14,7 +14,7 @@ impl HeaderType {
     }
 }
 
-enum Header {
+pub(crate) enum Header {
     Standard,
     PciToPciBridge,
     CardBusBridge,
