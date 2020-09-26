147fa620d3423d212ca00e5ef11dbe5045177360
toku-sa-n <tokusan441@gmail.com>
2020-09-26 12:24:03 +0900

    Define `Id` struct

--- src/space/header_spec/non_bridge/id.rs
+++ src/space/header_spec/non_bridge/id.rs
@@ -2,5 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+struct Id {
+    subsystem_vendor_id: SubsystemVendorId,
+    subsystem_id: SubsystemId,
+}
+
 define_field!(SubsystemVendorId, u16, 0x0b, 0, 0xffff);
 define_field!(SubsystemId, u16, 0x0b, 16, 0xffff);
