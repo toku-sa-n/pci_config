ee1894beaf216820f55ce1cf604f124bf3bd1ec8
toku-sa-n <tokusan441@gmail.com>
2020-09-26 17:47:06 +0900

    Define `MessageAddress::parse_registers`

--- src/space/header_spec/capability/register/spec/msi.rs
+++ src/space/header_spec/capability/register/spec/msi.rs
@@ -2,7 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use super::MessageControl;
+use {
+    super::MessageControl,
+    crate::space::{accessor::RegisterIndex, registers::Registers},
+};
 
 struct TypeSpecMsi {
     message_control: MessageControl,
@@ -15,6 +18,13 @@ struct MessageAddress(u64);
 impl MessageAddress {
     fn new(address: u64) -> Self {
         Self(address)
+    }
+
+    fn parse_registers(registers: &Registers, base: RegisterIndex) -> Self {
+        let lower = registers[base + 1] as u64;
+        let upper = registers[base + 2] as u64;
+
+        Self::new(upper << 32 | lower)
     }
 }
 
