737cbe2e7599b047af58ea676c969e2237c11d37
toku-sa-n <tokusan441@gmail.com>
2020-09-26 12:23:36 +0900

    Define fields

--- src/space/header_spec/non_bridge/id.rs
+++ src/space/header_spec/non_bridge/id.rs
@@ -1,3 +1,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+define_field!(SubsystemVendorId, u16, 0x0b, 0, 0xffff);
+define_field!(SubsystemId, u16, 0x0b, 16, 0xffff);
