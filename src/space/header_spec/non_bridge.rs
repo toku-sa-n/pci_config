51fda138fe300489e43a84bb652b63024b3e903e
toku-sa-n <tokusan441@gmail.com>
2020-09-26 12:17:17 +0900

    Define fields used in non-bridge PIC

--- src/space/header_spec/non_bridge.rs
+++ src/space/header_spec/non_bridge.rs
@@ -1,3 +1,10 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+define_field!(CardbusCisPointer, u32, 0x0a, 0, 0xffff_ffff);
+define_field!(ExpansionRomBaseAddress, u32, 0x0c, 0, 0xffff_ffff);
+define_field!(InterruptLine, u8, 0x0f, 0, 0xff);
+define_field!(InterruptPin, u8, 0x0f, 8, 0xff);
+define_field!(MinGrant, u8, 0x0f, 16, 0xff);
+define_field!(MaxLatency, u8, 0x0f, 24, 0xff);
