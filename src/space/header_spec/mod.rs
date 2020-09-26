749d2d0fd42570041e3f96b60d1278c84f06df57
toku-sa-n <tokusan441@gmail.com>
2020-09-26 12:32:12 +0900

    Define `HeaderSpec`

--- src/space/header_spec/mod.rs
+++ src/space/header_spec/mod.rs
@@ -5,6 +5,12 @@
 mod bar;
 mod non_bridge;
 
+use non_bridge::HeaderSpecNonBridge;
+
+enum HeaderSpec {
+    NonBridge(HeaderSpecNonBridge),
+}
+
 define_field!(CapabilitiesPointer, u8, 0x0d, 0, 0xff);
 define_field!(InterruptLine, u8, 0x0f, 0, 0xff);
 define_field!(InterruptPin, u8, 0x0f, 8, 0xff);
