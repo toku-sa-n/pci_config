e48953fbcb3c727f8ac8cb68b5d8a0653876a4e5
toku-sa-n <tokusan441@gmail.com>
2020-09-26 17:28:56 +0900

    Define `TypeSpecMsi`

--- src/space/header_spec/capability/register/spec/msi.rs
+++ src/space/header_spec/capability/register/spec/msi.rs
@@ -2,6 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use super::MessageControl;
+
+struct TypeSpecMsi {
+    message_control: MessageControl,
+    message_address: MessageAddress,
+    message_data: MessageData,
+}
+
 #[derive(Copy, Clone)]
 struct MessageAddress(u64);
 impl MessageAddress {
