4b4d73502bb82a96158e6975ec269e903635513f
toku-sa-n <tokusan441@gmail.com>
2020-09-26 13:58:54 +0900

    Define `Bar::parse_registers`

--- src/space/header_spec/bar.rs
+++ src/space/header_spec/bar.rs
@@ -2,9 +2,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use crate::space::{accessor::RegisterIndex, registers::Registers};
+
 #[derive(Copy, Clone)]
 pub(crate) struct Bar(u32);
 impl Bar {
+    fn parse_registers(registers: &Registers, index: Index) -> Self {
+        Self::new(registers[RegisterIndex::new(4 + index.as_usize() as u8)])
+    }
+
     fn new(bar: u32) -> Self {
         Self(bar)
     }
@@ -16,5 +22,9 @@ impl Index {
     fn new(index: usize) -> Self {
         assert!(index < 6);
         Self(index)
+    }
+
+    fn as_usize(self) -> usize {
+        self.0
     }
 }
