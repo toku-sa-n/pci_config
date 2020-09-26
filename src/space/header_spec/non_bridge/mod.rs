3626abb02796f64553df4f644bf442ffa135320c
toku-sa-n <tokusan441@gmail.com>
2020-09-26 12:22:28 +0900

    Add `id` module

--- src/space/header_spec/non_bridge/mod.rs
+++ src/space/header_spec/non_bridge/mod.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+mod id;
+
 define_field!(CardbusCisPointer, u32, 0x0a, 0, 0xffff_ffff);
 define_field!(ExpansionRomBaseAddress, u32, 0x0c, 0, 0xffff_ffff);
 define_field!(MinGrant, u8, 0x0f, 16, 0xff);
