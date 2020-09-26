8c759c70e70395d687a5e6b2ab3ef624b9b6edb3
toku-sa-n <tokusan441@gmail.com>
2020-09-26 14:07:15 +0900

    Define `Id::parse_registers`

--- src/space/header_spec/non_bridge/id.rs
+++ src/space/header_spec/non_bridge/id.rs
@@ -2,12 +2,21 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use crate::space::registers::Registers;
+
 pub(crate) struct Id {
     subsystem_vendor_id: SubsystemVendorId,
     subsystem_id: SubsystemId,
 }
 
 impl Id {
+    pub(crate) fn parse_registers(registers: &Registers) -> Self {
+        let subsystem_vendor_id = SubsystemVendorId::parse_registers(registers);
+        let subsystem_id = SubsystemId::parse_registers(registers);
+
+        Self::new(subsystem_vendor_id, subsystem_id)
+    }
+
     fn new(subsystem_vendor_id: SubsystemVendorId, subsystem_id: SubsystemId) -> Self {
         Self {
             subsystem_vendor_id,
