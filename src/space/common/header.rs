92d7ecbe068784062fb147d09494e65fdd13a111
toku-sa-n <tokusan441@gmail.com>
2020-09-26 14:24:03 +0900

    Define `headerType::ty`

--- src/space/common/header.rs
+++ src/space/common/header.rs
@@ -3,6 +3,16 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 define_field!(HeaderType, u8, 3, 16, 0xff);
+impl HeaderType {
+    fn ty(self) -> Header {
+        match self.0 & 0x7f {
+            0 => Header::Standard,
+            1 => Header::PciToPciBridge,
+            2 => Header::CardBusBridge,
+            _ => unreachable!(),
+        }
+    }
+}
 
 enum Header {
     Standard,
