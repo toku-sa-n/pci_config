ec536599a4a6bdf2988a7afc6e3b017bfd5389ef
toku-sa-n <tokusan441@gmail.com>
2020-09-27 23:36:15 +0900

    Add a doc comment

--- src/space/header_spec/capability/register/spec/msi_x.rs
+++ src/space/header_spec/capability/register/spec/msi_x.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+//! Fields of a capability register which is MSI-X specific.
+
 /// A module containing codes about MSI-X.
 use {
     super::MessageControl,
