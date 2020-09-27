cf179c932e51c3c44c3d2ff262a8c8afb3370041
toku-sa-n <tokusan441@gmail.com>
2020-09-27 19:01:14 +0900

    Add a doc comment

--- src/space/header_spec/capability/register/common.rs
+++ src/space/header_spec/capability/register/common.rs
@@ -2,8 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+//! Fields which all capability registers have.
 use crate::space::{accessor::RegisterIndex, registers::Registers};
 
+/// A structure providing information values which all capability registers have.
 pub(crate) struct Common {
     id: Id,
     next_pointer: NextPointer,
