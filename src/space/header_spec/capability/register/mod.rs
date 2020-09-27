d1c16cc2701a2b2c1afc21c3b4cc7ae33ba95887
toku-sa-n <tokusan441@gmail.com>
2020-09-27 18:39:15 +0900

    Add comments

--- src/space/header_spec/capability/register/mod.rs
+++ src/space/header_spec/capability/register/mod.rs
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-mod common;
-mod spec;
+pub mod common;
+pub mod spec;
 
 use {
     crate::space::{accessor::RegisterIndex, registers::Registers},
--- src/space/header_spec/capability/register/spec/msi_x.rs
+++ src/space/header_spec/capability/register/spec/msi_x.rs
@@ -2,6 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+/// A module containing codes about MSI-X.
 use {
     super::MessageControl,
     crate::space::{accessor::RegisterIndex, header_spec::bar, registers::Registers},
