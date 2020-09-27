797eefa37eb0a7ab8b363ba88939b1f01407ce1b
toku-sa-n <tokusan441@gmail.com>
2020-09-27 19:25:40 +0900

    Add a doc comment

--- src/space/header_spec/capability/register/spec/msi.rs
+++ src/space/header_spec/capability/register/spec/msi.rs
@@ -2,12 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+//! Fields of a capability register which is MSI-specific.
+
 use {
     super::MessageControl,
     crate::space::{accessor::RegisterIndex, registers::Registers},
 };
 
-pub(crate) struct TypeSpecMsi<'a> {
+pub struct TypeSpecMsi<'a> {
     registers: &'a Registers,
     base: RegisterIndex,
 }
