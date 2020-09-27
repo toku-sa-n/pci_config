f057a41cc0d4f806c4609670830956a9f8c49d1e
toku-sa-n <tokusan441@gmail.com>
2020-09-27 18:47:51 +0900

    Add docs

--- src/space/header_spec/bar.rs
+++ src/space/header_spec/bar.rs
@@ -2,10 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+//! A module containing data structures of Base Address Registers (BAR).
 use crate::space::{accessor::RegisterIndex, registers::Registers};
 
-/// Base Address Registers (BAR)
-
+/// Base Address Registers (BAR).
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Default)]
 pub struct Bar(u32);
 impl Bar {
@@ -14,9 +14,15 @@ impl Bar {
     }
 }
 
+/// A structure used to specify which BAR to use.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Default)]
 pub struct Index(usize);
 impl Index {
+    /// Create an instance of this type.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `index >= 6` as PCI configuration space never has more than 5 BARs.
     pub fn new(index: usize) -> Self {
         assert!(index < 6);
         Self(index)
