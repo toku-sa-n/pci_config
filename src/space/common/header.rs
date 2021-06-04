8540b6cafeaa683d532ec1ebb5bf69c4f710e316
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 16:08:25 +0900

    feat(HeaderType): define a method
    `multiple_functions_supported`.

--- src/space/common/header.rs
+++ src/space/common/header.rs
@@ -1,5 +1,12 @@
+use bit_field::BitField;
+
 define_field!(HeaderType, u8, 3, 16, 0xff);
 impl HeaderType {
+    /// Returns [`true`] if the device has multiple functions, and [`false`] otherwise.
+    pub fn multiple_functions_supported(&self) -> bool {
+        self.0.get_bit(7)
+    }
+
     pub(crate) fn ty(self) -> Header {
         match self.0 & 0x7f {
             0 => Header::Standard,
